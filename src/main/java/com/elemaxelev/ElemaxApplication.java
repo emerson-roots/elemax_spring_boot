ea8f674ac745ff20ea0abbce39da56696debcdd1
Emerson <emerson_sardinha@hotmail.com>
2021-08-10 21:17:38 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/elemaxelev/ElemaxApplication.java
@@ -0,0 +1,13 @@
+package com.elemaxelev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElemaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElemaxApplication.class, args);
+	}
+
+}
